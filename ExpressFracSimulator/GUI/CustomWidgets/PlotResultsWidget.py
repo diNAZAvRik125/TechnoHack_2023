c95322be94ff8fd5e2bdb5b68ce02fd6b836543b
Коновалов Назар Андреевич <n.konovalov1@g.nsu.ru>
2023-02-01 10:04:37 +0700

    2 graficis do

--- ExpressFracSimulator/GUI/CustomWidgets/PlotResultsWidget.py
+++ ExpressFracSimulator/GUI/CustomWidgets/PlotResultsWidget.py
@@ -2,6 +2,7 @@ from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QWidget,
     QVBoxLayout,
+    QHBoxLayout,
     QSizePolicy,
     QSplitter
 )
@@ -13,13 +14,13 @@ class PlotResultsWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
-        self.layout = QVBoxLayout()
+        self.layout = QHBoxLayout()
         self.graph_splitter = QSplitter(Qt.Vertical)
 
         # ------------------------------------------------------
         # Effective width plot widget
         # ------------------------------------------------------
-        self.effective_width_plot = MplWidget(
+        self.effective_width_plot_1 = MplWidget(
             self,
             figsize=(6, 4),
             xlabel="$x$",
@@ -28,15 +29,28 @@ class PlotResultsWidget(QWidget):
             is_grid=True,
             is_autoscale=True
         )
-        self.effective_width_plot.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.effective_width_plot_1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+
+        self.effective_width_plot_2 = MplWidget(
+            self,
+            figsize=(6, 4),
+            xlabel="$x$",
+            ylabel="$\\bar{w}$",
+            title="fluid",
+            is_grid=True,
+            is_autoscale=True
+        )
+        self.effective_width_plot_2.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
         # Plot reference to update graph
-        self._effective_width_plot_ref = None
+        self._effective_width_plot_ref_1 = None
+        self._effective_width_plot_ref_2 = None
 
         # ------------------------------------------------------
         # Insert plot widgets to splitter
         # ------------------------------------------------------
-        self.graph_splitter.addWidget(self.effective_width_plot)
+        self.graph_splitter.addWidget(self.effective_width_plot_1)
+        self.graph_splitter.addWidget(self.effective_width_plot_2)
 
         self.layout.addWidget(self.graph_splitter)
         self.setLayout(self.layout)
@@ -48,18 +62,34 @@ class PlotResultsWidget(QWidget):
         # ------------------------------------------------------
         # 1D Plot
         # ------------------------------------------------------
-        if self._effective_width_plot_ref is None:
-            self._effective_width_plot_ref = self.effective_width_plot.canvas.ax.plot(x, width, 'r')[0]
+        if self._effective_width_plot_ref_1 is None:
+            self._effective_width_plot_ref_1 = self.effective_width_plot_1.canvas.ax.plot(x, width, 'r')[0]
         else:
-            self._effective_width_plot_ref.set_xdata(x)
-            self._effective_width_plot_ref.set_ydata(width)
-        self.effective_width_plot.canvas.draw()
+            self._effective_width_plot_ref_1.set_xdata(x)
+            self._effective_width_plot_ref_1.set_ydata(width)
+        self.effective_width_plot_1.canvas.draw()
+
+        y = np.array(results_data[time_index]["mesh"]["dx"])
+        fluid = np.array(results_data[time_index]["results"]["fluid_pressure"])
+        # ------------------------------------------------------
+        # 1D Plot
+        # ------------------------------------------------------
+        if self._effective_width_plot_ref_2 is None:
+            self._effective_width_plot_ref_2 = self.effective_width_plot_2.canvas.ax.plot(y, fluid, 'r')[0]
+        else:
+            self._effective_width_plot_ref_2.set_xdata(y)
+            self._effective_width_plot_ref_2.set_ydata(fluid)
+        self.effective_width_plot_2.canvas.draw()
 
     def clear_plots(self):
         # ------------------------------------------------------
         # 1D Plot
         # ------------------------------------------------------
-        if self._effective_width_plot_ref is not None:
-            self._effective_width_plot_ref.remove()
-            self._effective_width_plot_ref = None
+        if self._effective_width_plot_ref_1 is not None:
+            self._effective_width_plot_ref_1.remove()
+            self._effective_width_plot_ref_1 = None
+
+        if self._effective_width_plot_ref_2 is not None:
+            self._effective_width_plot_ref_2.remove()
+            self._effective_width_plot_ref_2 = None
         self.effective_width_plot.canvas.draw()
